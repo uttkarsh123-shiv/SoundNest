7591334050c68d178a88eb0afea47174676ba51b
Uttkarsh Singh <uttkarshsingh450@gmail.com>
2024-09-18 17:36:06 +0530

    eslint

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,11 @@ const nextConfig = {
     typescript:{
         ignoreBuildErrors: true,
     },
+    eslint: {
+        // Enable ESLint
+        ignoreDuringBuilds: true, // Ignore ESLint during production builds
+        dirs: ['app', 'components'], // Specify the directories to lint
+    },
     images: {
         remotePatterns: [
             {
