550423e2112ed1bd3f1deaec64c7485e1d28c3de
Uttkarsh Singh <uttkarshsingh450@gmail.com>
2025-02-19 02:12:04 +0530

    fixed zod

--- app/(root)/create-podcast/page.tsx
+++ app/(root)/create-podcast/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
 import { useForm } from "react-hook-form";
 import { Button } from "@/components/ui/button";
 import {
